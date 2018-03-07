9688349167702bcc7286396e7d8312fa11b411a6
julianleahy <julianleahynz@gmail.com>
2018-03-07 23:40:35 +1300

    change path for image and gpx files

--- src/app/shared/activities.ts
+++ src/app/shared/activities.ts
@@ -7,7 +7,7 @@ export const SAVED_ACTIVITIES: IActivity[] = [
     'date': new Date('01/01/2018'),
     'distance': 7.3,
     'comments': 'Great flowing trails',
-    'gpxData': '../../assets/gpx/1.gpx'
+    'gpxData': 'assets/gpx/1.gpx'
 
   },
   {
@@ -16,7 +16,7 @@ export const SAVED_ACTIVITIES: IActivity[] = [
     'date': new Date('01/09/2018'),
     'distance': 4.2,
     'comments': 'Tough ride, big climb!',
-    'gpxData': '../../assets/gpx/2.gpx'
+    'gpxData': 'assets/gpx/2.gpx'
 
   },
   {
@@ -25,7 +25,7 @@ export const SAVED_ACTIVITIES: IActivity[] = [
     'date': new Date('01/21/2018'),
     'distance': 5.8,
     'comments': 'Great ride down!',
-    'gpxData': '../../assets/gpx/3.gpx'
+    'gpxData': 'assets/gpx/3.gpx'
 
   },
   {
@@ -34,7 +34,7 @@ export const SAVED_ACTIVITIES: IActivity[] = [
     'date': new Date('01/26/2018'),
     'distance': 15.6,
     'comments': 'Beautiful day for the road bike',
-    'gpxData': '../../assets/gpx/4.gpx'
+    'gpxData': 'assets/gpx/4.gpx'
 
   },
 ];
