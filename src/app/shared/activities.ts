f0fcf893a762e8c723e690fd57e551a92de9a417
julianleahy <julianleahynz@gmail.com>
2018-03-07 19:27:12 +1300

    change dates as they are US formatted

--- src/app/shared/activities.ts
+++ src/app/shared/activities.ts
@@ -4,7 +4,7 @@ export const SAVED_ACTIVITIES: IActivity[] = [
   {
     'id': 1,
     'name': 'Blue Hill Trail',
-    'date': new Date('06/01/2018'),
+    'date': new Date('01/01/2018'),
     'distance': 6.3,
     'comments': 'Great easy ride',
     'gpxData': '../../assets/gpx/1.gpx'
@@ -13,7 +13,7 @@ export const SAVED_ACTIVITIES: IActivity[] = [
   {
     'id': 2,
     'name': 'Rocky Road Trails',
-    'date': new Date('12/01/2018'),
+    'date': new Date('01/09/2018'),
     'distance': 10.7,
     'comments': 'Tough ride, Big climbs!',
     'gpxData': '../../assets/gpx/2.gpx'
@@ -22,7 +22,7 @@ export const SAVED_ACTIVITIES: IActivity[] = [
   {
     'id': 3,
     'name': 'Senty Post',
-    'date': new Date('18/01/2018'),
+    'date': new Date('01/21/2018'),
     'distance': 7.0,
     'comments': 'Long winding trails!',
     'gpxData': '../../assets/gpx/3.gpx'
@@ -31,7 +31,7 @@ export const SAVED_ACTIVITIES: IActivity[] = [
   {
     'id': 4,
     'name': 'Black Cross',
-    'date': new Date('29/01/2018'),
+    'date': new Date('01/26/2018'),
     'distance': 12.3,
     'comments': 'Very technical ride',
     'gpxData': '../../assets/gpx/4.gpx'
