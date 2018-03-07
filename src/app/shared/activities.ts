5c8f0eea5f2416f1c7dfe333786329a7b707bacb
julianleahy <julianleahynz@gmail.com>
2018-03-07 22:34:08 +1300

    text changes

--- src/app/shared/activities.ts
+++ src/app/shared/activities.ts
@@ -6,22 +6,22 @@ export const SAVED_ACTIVITIES: IActivity[] = [
     'name': 'Makara Peak',
     'date': new Date('01/01/2018'),
     'distance': 7.3,
-    'comments': 'Great Flowing trails',
+    'comments': 'Great flowing trails',
     'gpxData': '../../assets/gpx/1.gpx'
 
   },
   {
     'id': 2,
-    'name': 'Mount Kaukau',
+    'name': 'Mt. Kaukau',
     'date': new Date('01/09/2018'),
     'distance': 4.2,
-    'comments': 'Tough ride, Big climbs!',
+    'comments': 'Tough ride, big climb!',
     'gpxData': '../../assets/gpx/2.gpx'
 
   },
   {
     'id': 3,
-    'name': 'Mount Victoria',
+    'name': 'Mt. Victoria',
     'date': new Date('01/21/2018'),
     'distance': 5.8,
     'comments': 'Great run to the Botanical Gardens!',
@@ -30,7 +30,7 @@ export const SAVED_ACTIVITIES: IActivity[] = [
   },
   {
     'id': 4,
-    'name': 'Ride to the Coast',
+    'name': 'Ride to the coast',
     'date': new Date('01/26/2018'),
     'distance': 15.6,
     'comments': 'Beautiful day for the road bike',
